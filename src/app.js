07bdd29062161f38affaff3a75b3993da999d1d6
Rasmus Povlsen <git@rpovlsen.com>
2021-03-23 12:26:01 +0100

    Done v1

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import { join } from 'path';
+import Collection from './db/db';
 
 const app = express();
 const PORT = process.env.PORT || 80;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(join(__dirname, '..', 'public')));
 
-app.listen(PORT, () => {
+async function main() {
+
+
+  app.listen(PORT, () => {
     console.clear();
     console.log(`Listening on ${PORT}...`);
-});
+  });
+}
+
+main();
